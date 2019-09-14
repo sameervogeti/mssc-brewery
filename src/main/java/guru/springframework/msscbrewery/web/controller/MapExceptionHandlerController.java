56fbabf7bd9134eb6dec809adc59f1d708a26064
svogeti <sameervogeti@gmail.com>
2019-09-15 00:01:42 +0530

    Added mapstruct dependency

--- src/main/java/guru/springframework/msscbrewery/web/controller/MapExceptionHandlerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/MapExceptionHandlerController.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbrewery.web.controller;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +23,10 @@ public class MapExceptionHandlerController {
             errors.add(constraintViolation.getPropertyPath() + ": " + constraintViolation.getMessage());
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List> bindErrorHandler(BindException e) {
+        return new ResponseEntity<>(e.getAllErrors(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -2,7 +2,9 @@ package guru.springframework.msscbrewery.web.controller.v2;
 
 import guru.springframework.msscbrewery.services.v2.BeerServiceV2;
 import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
-import lombok.extern.java.Log;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +18,12 @@ import java.util.UUID;
  */
 @RequestMapping("/api/v2/beer")
 @RestController
-@Log
+@Slf4j
+@RequiredArgsConstructor
 public class BeerControllerV2 {
     private final BeerServiceV2 beerServiceV2;
 
-    public BeerControllerV2(BeerServiceV2 beerServiceV2) {
-        this.beerServiceV2 = beerServiceV2;
-    }
+
 
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId) {
@@ -36,9 +37,9 @@ public class BeerControllerV2 {
         log.info("Inside Post");
 
 
-        BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
+        val savedDto = beerServiceV2.saveNewBeer(beerDto);
 
-        HttpHeaders headers = new HttpHeaders();
+        val headers = new HttpHeaders();
         //todo add hostname to url
         headers.add("Location", "/api/v1/beer/");
 
